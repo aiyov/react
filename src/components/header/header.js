f8c88120eda39ec0d8078db91b8ed81d6e675bab
aiyov <newobjfoxmail.com>
2018-05-24 23:07:57 +0800

    完善头部

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,104 +1,118 @@
 import React, {Component} from 'react';
-import seller from '../../../data.json'
+import data from '../../data.json';
+import './header.styl';
 
-class App extends Component {
-  constructor(props){
-    super(props);
-    this.showDetail = this.showDetail.bind(this);
-    this.hideDetail = this.hideDetail.bind(this);
-    this.state = {
-      detailShow: false,
-      seller,
+export default class Header extends Component {
+    constructor(props) {
+        super(props);
+        this.showDetail = this.showDetail.bind(this);
+        this.hideDetail = this.hideDetail.bind(this);
+        this.state = {
+            classNameMap: ['decrease', 'discount', 'special', 'invoice', 'guarantee'],
+            detailShow: false,
+            seller: data.seller,
+        }
     }
-  }
-  showDetail() {
-    this.detailShow = true;
-  }
-  hideDetail() {
-    this.detailShow = false;
-  }
 
-  render() {
-    return (
-      <div className="header">
-        <div className="content-wrapper">
-          <div className="avatar">
-            <img width="64" height="64" src={seller.avatar}/>
-          </div>
-          <div className="content">
-            <div className="title">
-              <span className="brand"></span>
-              <span className="name">{seller.name}</span>
-            </div>
-            <div className="description">
-              {seller.description}/{seller.deliveryTime}分钟送达
-            </div>
-            {
-              seller.supports ? (
-                <div className="support">
-                  <span className="icon" classNameName={classNameMap[seller.supports[0].type]}></span>
-                  <span className="text">{seller.supports[0].description}</span>
-                </div>
-              ) : null
-            }
-          </div>
-          {seller.supports ? (
-            <div className="support-count" onClick={showDetail}>
-              <span className="count">{seller.supports.length}个</span>
-              <i className="icon-keyboard_arrow_right"></i>
-            </div>
-          ) : null}
+    showDetail() {
+        this.setState({
+            detailShow: true
+        })
+    }
 
-        </div>
-        <div className="bulletin-wrapper" onClick={showDetail}>
-          <span className="bulletin-title"></span>
-          <span className="bulletin-text">{seller.bulletin}</span>
-          <i className="icon-keyboard_arrow_right"></i>
-        </div>
-        <div className="background">
-          <img src={seller.avatar} width="100%" height="100%"/>
-        </div>
-        <transition name="fade">
-          <div v-show="detailShow" className="detail">
-            <div className="detail-wrapper clearfix">
-              <div className="detail-main">
-                <h1 className="name">{seller.name}</h1>
-                <div className="star-wrapper">
-                  {/*<star size={48} score={seller.score}></star>*/}
+    hideDetail() {
+        this.setState({
+            detailShow: false
+        })
+    }
+
+    componentDidMount() {
+        console.log(data)
+    }
+
+    render() {
+        return (
+            <div className="header">
+                <div className="content-wrapper">
+                    <div className="avatar">
+                        <img alt="bei" width="64" height="64" src={this.state.seller.avatar}/>
+                    </div>
+                    <div className="content">
+                        <div className="title">
+                            <span className="brand"></span>
+                            <span className="name">{this.state.seller.name}</span>
+                        </div>
+                        <div className="description">
+                            {this.state.seller.description}/{this.state.seller.deliveryTime}分钟送达
+                        </div>
+                        {
+                            this.state.seller.supports ? (
+                                <div className="support">
+                                    <span className={this.state.classNameMap[this.state.seller.supports[0].type]+' icon'}></span>
+                                    <span className="text">{this.state.seller.supports[0].description}</span>
+                                </div>
+                            ) : null
+                        }
+                    </div>
+                    {this.state.seller.supports ? (
+                        <div className="support-count" onClick={this.showDetail}>
+                            <span className="count">{this.state.seller.supports.length}个</span>
+                            <i className="icon-keyboard_arrow_right"></i>
+                        </div>
+                    ) : null}
+
                 </div>
-                <div className="title">
-                  <div className="line"></div>
-                  <div className="text">优惠信息</div>
-                  <div className="line"></div>
+                <div className="bulletin-wrapper" onClick={this.showDetail}>
+                    <span className="bulletin-title"></span>
+                    <span className="bulletin-text">{this.state.seller.bulletin}</span>
+                    <i className="icon-keyboard_arrow_right"></i>
                 </div>
-                {seller.supports ? (
-                  <ul className="supports">
-                    {seller.supports.map((item, index)=>{
-                      return (
-                        <li className="support-item">
-                          <span className="icon" className={classNameMap[seller.supports[index].type]}></span>
-                          <span className="text">{seller.supports[index].description}</span>
-                        </li>
-                      )
-                    })}
-                  </ul>
-                ) : null}
-                <div className="title">
-                  <div className="line"></div>
-                  <div className="text">商家公告</div>
-                  <div className="line"></div>
+                <div className="background">
+                    <img src={this.state.seller.avatar} alt="背景" width="100%" height="100%"/>
                 </div>
-                <div className="bulletin">
-                  <p className="content">{seller.bulletin}</p>
-                </div>
-              </div>
+                {this.state.detailShow?(
+                    <div className="detail">
+                        <div className="detail-wrapper clearfix">
+                            <div className="detail-main">
+                                <h1 className="name">{this.state.seller.name}</h1>
+                                <div className="star-wrapper">
+                                    {/*<star size={48} score={seller.score}></star>*/}
+                                </div>
+                                <div className="title">
+                                    <div className="line"></div>
+                                    <div className="text">优惠信息</div>
+                                    <div className="line"></div>
+                                </div>
+                                {this.state.seller.supports ? (
+                                    <ul className="supports">
+                                        {this.state.seller.supports.map((item, index) => {
+                                            return (
+                                                <li key={index} className="support-item">
+                                                <span
+                                                    className={this.state.classNameMap[this.state.seller.supports[index].type] + " icon"}></span>
+                                                    <span
+                                                        className="text">{this.state.seller.supports[index].description}</span>
+                                                </li>
+                                            )
+                                        })}
+                                    </ul>
+                                ) : null}
+                                <div className="title">
+                                    <div className="line"></div>
+                                    <div className="text">商家公告</div>
+                                    <div className="line"></div>
+                                </div>
+                                <div className="bulletin">
+                                    <p className="content">{this.state.seller.bulletin}</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="detail-close" onClick={this.hideDetail}>
+                            <i className="icon-close"></i>
+                        </div>
+                    </div>
+                ):null}
             </div>
-            <div className="detail-close" onClick={hideDetail}>
-              <i className="icon-close"></i>
-            </div>
-          </div>
-        </transition>
-      </div>
-    )
-  }
+        )
+    }
 }
