cd12a48500bb3426c5c6e8e0bad94c87955274cb
aiyov <newobjfoxmail.com>
2018-06-10 23:03:04 +0800

    mock ajax

---
+++ src/api/shop.js
@@ -0,0 +1,9 @@
+import data from './data.json';
+
+const _products = data.seller;
+const TIMEOUT = 100;
+
+export default {
+    getProducts: (cb, timeout) => setTimeout(() => cb(_products), timeout || TIMEOUT),
+    buyProducts: (payload, cb, timeout) => setTimeout(() => cb(), timeout || TIMEOUT)
+}
